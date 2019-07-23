20adad1f7bc965d4c7695a03073e52eb81fc39d6
odin <odin@Odin>
2019-07-23 21:53:03 +0100

    add Eportugal

--- src/test/java/learnmake/automation/pages/Eportugal.java
+++ src/test/java/learnmake/automation/pages/Eportugal.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
+ 
 public class Eportugal extends AbstractPage {
 
     private static final String PAGE_HOME_URL = "https://www.google.com/";
